475b39ddd81ddf79a1edffc7bd91b129792d9b41
Utkarsh Kashyap <utkarsh.k05@gmail.com>
2017-02-20 22:39:34 +0530

    Binary data extractor

--- utilities.py
+++ utilities.py
@@ -79,6 +79,22 @@ def getImage(limit_to = 40000):
     X = X.reshape(N, 1, d, d)
     return X, Y
 
+def getBinaryData():
+    Y = []
+    X = []
+    first = True
+    for line in open('fer2013.csv'):
+        if first:
+            first = False
+        else:
+            row = line.split(',')
+            y = int(row[0])
+            if y == 0 or y == 1:
+                Y.append(y)
+                X.append([int(p) for p in row[1].split()])
+
+    return np.array(X) / 255.0, np.array(Y)
+
 
 def labelCodes():
     labels =  {

c92c009894a50dfb8b24e4a488cdaa25e97e21c6
Utkarsh Kashyap <utkarsh.k05@gmail.com>
2017-02-20 22:36:26 +0530

    Basic CNN utilities

--- utilities.py
+++ utilities.py
@@ -1,6 +1,48 @@
 import numpy as np
 import pandas as pd
 
+def init_weight_and_bias(M1, M2):
+    W = np.random.randn(M1, M2) / np.sqrt(M1 + M2)
+    b = np.zeros(M2)
+    return W.astype(np.float32), b.astype(np.float32)
+
+def init_filter(shape, poolsize):
+    w = np.random.randn(*shape) / np.sqrt(np.prod(shape[1:]) + shape[0]*np.prod(shape[2:] / np.prod(poolsize)))
+    return w.astype(np.float32)
+
+def relu(x):
+    # f(x) = max(0, x)
+    return x * (x > 0)
+
+def sigmoid(A):
+    return 1 / (1 + np.exp(-A))
+
+def softmax(A):
+    expA = np.exp(A)
+    return expA / expA.sum(axis = 1, keepdims = True)
+
+def sigmoid_cost(T, Y):
+    return -(T*np.log(Y) + (1-T)*np.log(1-Y)).sum()
+
+def cost(T, Y):
+    return -(T*np.log(Y)).sum()
+
+def cost2(T, Y):
+    #uses targets to index Y instead of multiplying by a matrix of many 0s
+    N = len(T)
+    return -np.log(Y[np.arrange(N), T]).mean()
+
+def error_rate(targets, predictions):
+    return np.mean(target != predictions)
+
+def y2indicator(y):
+    N = len(y)
+    K = len(set(y))
+    ind = np.zeros((N, K))
+    for i in xrange(N):
+        ind[i, y[i]] = 1
+    return ind
+
 def getRawData(balance_ones = True, limit_count = 40000):
     #Image Size: 48x48
     expressions = []
@@ -21,7 +63,12 @@ def getRawData(balance_ones = True, limit_count = 40000):
     expressions = np.array(expressions)
 
     if balance_ones:
-        pass #class balancing later
+        #clas imbalance problem
+        X1, Y1 = pixels[expressions != 1, :], expressions[expressions != 1]
+        X2 = pixels[expressions == 1, :]
+        X2 = np.repeat(X2, 9, axis = 0)
+        pixels = np.vstack([X1, X2])
+        expressions = np.concatenate((Y1, [1]*len(X1)))
 
     return pixels, expressions
 
@@ -31,6 +78,7 @@ def getImage(limit_to = 40000):
     d = int(np.sqrt(D))
     X = X.reshape(N, 1, d, d)
     return X, Y
+
 
 def labelCodes():
     labels =  {
